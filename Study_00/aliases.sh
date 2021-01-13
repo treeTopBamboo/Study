6c60b88c3f6b604c5027062f91f2b7ccaf07795b
treeTopBamboo <74302467+treeTopBamboo@users.noreply.github.com>
2021-01-13 17:17:13 +0900

    Delete aliases.sh

--- Study_00/aliases.sh
+++
@@ -1,8 +0,0 @@
-#イメージの作成とコンテナ起動
-#alias ショートカットコマンド="本来のコマンド
-#docker-compose up --build サービス名
-alias vue-pj="docker-compose up --build testapp"
-
-#コンテナ内のshファイルを起動
-#docker exec -it コンテナ名 sh
-alias vue-exec="docker exec -it vc3test0 sh"
